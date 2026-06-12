847fd659da1e733a56240d80e7e3e34a381c0bed
scarletsource <ivan.turuvnenko17@gmail.com>
2026-06-13 00:57:19 +0300

    Update pc_types.py

--- keyboards/pc_types.py
+++ keyboards/pc_types.py
@@ -1,1 +1,40 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+
+def pc_types_menu():
+
+    builder = InlineKeyboardBuilder()
+
+    builder.button(
+        text="🎮 Game Арена",
+        callback_data="type_1"
+    )
+
+    builder.button(
+        text="👑 VIP Арена",
+        callback_data="type_2"
+    )
+
+    builder.button(
+        text="💎 Comfort Арена",
+        callback_data="type_3"
+    )
+
+    builder.button(
+        text="📺 TV",
+        callback_data="type_4"
+    )
+
+    builder.button(
+        text="👥 DUO ROOM",
+        callback_data="type_5"
+    )
+
+    builder.button(
+        text="◀️ Назад",
+        callback_data="back_main"
+    )
+
+    builder.adjust(1)
+
+    return builder.as_markup()
