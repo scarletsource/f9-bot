f6e28a2e169329f61871b156a1f53a1300a1ac01
scarletsource <ivan.turuvnenko17@gmail.com>
2026-06-13 01:00:32 +0300

    Update pc_list.py

--- keyboards/pc_list.py
+++ keyboards/pc_list.py
@@ -1,1 +1,22 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+
+def pc_list_menu(pcs):
+
+    builder = InlineKeyboardBuilder()
+
+    for pc in pcs:
+
+        builder.button(
+            text=f"🖥 PC-{pc['name']}",
+            callback_data=f"pcid_{pc['UUID']}"
+        )
+
+    builder.button(
+        text="◀️ Назад",
+        callback_data="pc"
+    )
+
+    builder.adjust(2)
+
+    return builder.as_markup()
