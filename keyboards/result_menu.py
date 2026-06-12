d37a1b9d952e609ebc9b1ef9fe33f058bdeaf28a
scarletsource <ivan.turuvnenko17@gmail.com>
2026-06-13 01:45:45 +0300

    Update result_menu.py

--- keyboards/result_menu.py
+++ keyboards/result_menu.py
@@ -1,1 +1,20 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+
+def result_menu():
+
+    builder = InlineKeyboardBuilder()
+
+    builder.button(
+        text="🔙 К компьютерам",
+        callback_data="pc"
+    )
+
+    builder.button(
+        text="🏠 Главное меню",
+        callback_data="back_main"
+    )
+
+    builder.adjust(1)
+
+    return builder.as_markup()
