fa70a7031e6f95162c6208564257c47ab8719a76
scarletsource <ivan.turuvnenko17@gmail.com>
2026-06-12 23:45:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,13 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message: Message):
 
+```
 await message.answer(
     "👋 Добро пожаловать в F9 Кибер Арена\n\n"
     "Выберите раздел:",
     reply_markup=main_menu()
 )
+```
 
 @dp.message(Command("clubs"))
 async def clubs(message: Message):
