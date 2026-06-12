74aea6630abad29201264e909700c50ce45e1059
scarletsource <ivan.turuvnenko17@gmail.com>
2026-06-13 02:35:20 +0300

    Update pc_status.py

--- utils/pc_status.py
+++ utils/pc_status.py
@@ -1,14 +1,48 @@
-PC_STATUS = {}
+import json
+import os
+
+FILE_NAME = "pc_status.json"
+
+
+def load_statuses():
+
+    if not os.path.exists(FILE_NAME):
+
+        with open(FILE_NAME, "w", encoding="utf-8") as f:
+            json.dump({}, f)
+
+    with open(FILE_NAME, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def save_statuses(statuses):
+
+    with open(FILE_NAME, "w", encoding="utf-8") as f:
+        json.dump(
+            statuses,
+            f,
+            ensure_ascii=False,
+            indent=4
+        )
 
 
 def set_status(uuid, status):
 
-    PC_STATUS[uuid] = status
+    statuses = load_statuses()
+
+    statuses[uuid] = status
+
+    save_statuses(statuses)
 
 
 def get_status(uuid):
 
-    return PC_STATUS.get(uuid, "free")
+    statuses = load_statuses()
+
+    return statuses.get(
+        uuid,
+        "free"
+    )
 
 
 def get_status_icon(uuid):
@@ -27,7 +61,10 @@ def get_status_icon(uuid):
         "error": "🚨"
     }
 
-    return icons.get(status, "⚪")
+    return icons.get(
+        status,
+        "⚪"
+    )
 
 
 def get_status_name(uuid):
@@ -46,4 +83,7 @@ def get_status_name(uuid):
         "error": "Ошибка"
     }
 
-    return names.get(status, "Неизвестно")
+    return names.get(
+        status,
+        "Неизвестно"
+    )
