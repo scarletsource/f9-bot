f2955daf01e48adee49b70331008f0fcb2310f8c
scarletsource <ivan.turuvnenko17@gmail.com>
2026-06-13 01:07:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -81,24 +81,8 @@ async def show_pcs(callback: CallbackQuery):
 
     data = get_pc_linking()
 
-    pcs = []
-
-    for pc in data["data"]:
-
-        if pc["packets_type_PC"] == pc_type:
-
-            if pc["name"] is not None:
-
-                pcs.append(pc)
-
-    pcs = sorted(
-        pcs,
-        key=lambda x: int(x["name"])
-    )
-
-    await callback.message.edit_text(
-        "🖥 Выберите компьютер:",
-        reply_markup=pc_list_menu(pcs)
+    await callback.message.answer(
+        str(data)[:4000]
     )
 
     await callback.answer()
