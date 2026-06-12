168b44223423618b642027bf971239e64c007832
scarletsource <ivan.turuvnenko17@gmail.com>
2026-06-12 23:42:56 +0300

    Update menu.py

--- keyboards/menu.py
+++ keyboards/menu.py
@@ -1,26 +1,25 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-main_menu = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text="👤 Профиль"),
-            KeyboardButton(text="💰 Баланс")
-        ],
-        [
-            KeyboardButton(text="⭐ Бонусы"),
-            KeyboardButton(text="🎮 История")
-        ],
-        [
-            KeyboardButton(text="🥤 Склад"),
-            KeyboardButton(text="💵 Продажи")
-        ],
-        [
-            KeyboardButton(text="📊 Статистика"),
-            KeyboardButton(text="🖥 Компьютеры")
-        ],
-        [
-            KeyboardButton(text="⚙️ Админка")
-        ]
-    ],
-    resize_keyboard=True
-)
+def main_menu():
+
+```
+builder = InlineKeyboardBuilder()
+
+builder.button(text="👤 Профиль", callback_data="profile")
+builder.button(text="💰 Баланс", callback_data="balance")
+
+builder.button(text="⭐ Бонусы", callback_data="bonus")
+builder.button(text="🎮 История", callback_data="sessions")
+
+builder.button(text="🥤 Склад", callback_data="goods")
+builder.button(text="💵 Продажи", callback_data="sales")
+
+builder.button(text="📊 Статистика", callback_data="stats")
+builder.button(text="🖥 Компьютеры", callback_data="pc")
+
+builder.button(text="⚙️ Админка", callback_data="admin")
+
+builder.adjust(2, 2, 2, 2, 1)
+
+return builder.as_markup()
+```
