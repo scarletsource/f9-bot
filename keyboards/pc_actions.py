bf2033c185d9f0bc50e79c3c88992bda5003a30f
scarletsource <ivan.turuvnenko17@gmail.com>
2026-06-13 01:14:27 +0300

    Update pc_actions.py

--- keyboards/pc_actions.py
+++ keyboards/pc_actions.py
@@ -1,1 +1,40 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+
+def pc_actions_menu(uuid):
+
+    builder = InlineKeyboardBuilder()
+
+    builder.button(
+        text="🔄 Перезагрузить",
+        callback_data=f"reboot_{uuid}"
+    )
+
+    builder.button(
+        text="⚡ Включить",
+        callback_data=f"poweron_{uuid}"
+    )
+
+    builder.button(
+        text="🔒 Заблокировать",
+        callback_data=f"lock_{uuid}"
+    )
+
+    builder.button(
+        text="🔓 Разблокировать",
+        callback_data=f"unlock_{uuid}"
+    )
+
+    builder.button(
+        text="⛔ Выключить",
+        callback_data=f"poweroff_{uuid}"
+    )
+
+    builder.button(
+        text="◀️ Назад",
+        callback_data="pc"
+    )
+
+    builder.adjust(2, 2, 1, 1)
+
+    return builder.as_markup()
