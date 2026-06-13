1bde9f05e553d72c48331ec0ff2bc485a2016608
scarletsource <ivan.turuvnenko17@gmail.com>
2026-06-13 20:11:36 +0300

    Update pc_status.py

--- utils/pc_status.py
+++ utils/pc_status.py
@@ -2,6 +2,9 @@ import json
 import os
 
 from langame_api import get_busy_pcs
+from langame_api import get_pc_session
+
+from datetime import datetime
 
 BASE_DIR = os.path.dirname(
     os.path.abspath(__file__)
@@ -13,6 +16,46 @@ FILE_NAME = os.path.join(
 )
 
 print("STATUS FILE =", FILE_NAME)
+
+def get_pc_play_time(uuid):
+
+    session = get_pc_session(uuid)
+
+    if session is None:
+
+        return "-"
+
+    start = datetime.strptime(
+        session["date_start"],
+        "%Y-%m-%d %H:%M:%S"
+    )
+
+    delta = datetime.now() - start
+
+    hours = delta.seconds // 3600
+    minutes = (delta.seconds % 3600) // 60
+
+    return f"{hours}ч {minutes}м"
+
+def get_pc_start_time(uuid):
+
+    session = get_pc_session(uuid)
+
+    if session is None:
+
+        return "-"
+
+    return session["date_start"]
+
+def get_pc_user(uuid):
+
+    session = get_pc_session(uuid)
+
+    if session is None:
+
+        return "Свободен"
+
+    return f"ID {session['guest_id']}"
 
 def load_statuses():
 
