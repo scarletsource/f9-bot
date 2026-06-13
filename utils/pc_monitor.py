7089420b1276f74661fc7b1d3528eb787be9bcfd
scarletsource <ivan.turuvnenko17@gmail.com>
2026-06-14 00:20:25 +0300

    Update pc_monitor.py

--- utils/pc_monitor.py
+++ utils/pc_monitor.py
@@ -1,9 +1,31 @@
 import asyncio
 
+from langame_api import get_pc_linking
+
+
 async def monitor_pcs():
 
     while True:
 
-        print("МОНИТОР ОБНОВИЛСЯ")
+        try:
+
+            data = get_pc_linking()
+
+            print()
+            print("========== ПК КЛУБА ==========")
+
+            for pc in data["data"]:
+
+                print(
+                    pc["name"],
+                    pc["UUID"]
+                )
+
+            print("=============================")
+            print()
+
+        except Exception as e:
+
+            print("ОШИБКА МОНИТОРА:", e)
 
         await asyncio.sleep(5)
