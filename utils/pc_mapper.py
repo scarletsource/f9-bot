69067e6494190ad72b718959702bf40c12861d22
scarletsource <ivan.turuvnenko17@gmail.com>
2026-06-13 22:10:43 +0300

    Create pc_mapper.py

---
+++ utils/pc_mapper.py
@@ -0,0 +1,46 @@
+from langame_api import (
+    get_pc_linking,
+    get_guest_sessions
+)
+
+
+def get_real_session_by_pc(uuid):
+
+    sessions = get_guest_sessions()
+
+    if not sessions["status"]:
+
+        return None
+
+    for session in sessions["data"]:
+
+        if (
+            session["UUID"] == uuid
+            and session["date_stop"] is None
+        ):
+
+            return session
+
+    return None
+
+
+def get_real_status(uuid):
+
+    session = get_real_session_by_pc(uuid)
+
+    if session is None:
+
+        return "free"
+
+    return "session"
+
+
+def get_real_user(uuid):
+
+    session = get_real_session_by_pc(uuid)
+
+    if session is None:
+
+        return "Свободен"
+
+    return f"ID {session['guest_id']}"
